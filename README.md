82813e2dec2c8e96c66ab38ced57a76e2dae81be
Tiago Barbosa <tiago@farias.cc>
2021-09-22 23:11:05 -0300

    Ajustes no front e criação do README

---
+++ README.md
@@ -0,0 +1,55 @@
+URNA ELETRÔNICA
+---
+
+###Resumo
+
+Experimento e estudos de criação de API e um SPA para testes de consumo dos dados.
+
+###Etapas para realizar o teste
+
+- Faça a clonagem deste repositório
+- Abra a IDE que achar mais confortável para edição do código (Visual Studio, VS Code, RIDER etc).
+- No terminal integrado da IDE verifique se está com o SDK do .Net instalado usando o comando abaixo:
+  ```C#
+  dotnet --version
+  ```
+- Se o comando retornar algum resultado como **dotnet v5.#.#** poderemos continuar o processo de testes, caso dê algum erro, recomendo que baixe o SDK do .Net neste link https://dotnet.microsoft.com/download
+- Com o dotnet SDK instalado, novamente no Terminal integrado insira o código abaixo para ativar a API:
+  ```C#
+  dotnet run
+  ```
+- Nas pastas deste repositório encontram-se coleções de requisições REST, onde poderá utilizar em aplicativos como Insomnia, Postman, ThunderClient etc.
+- Após importar a coleção em seu aplicativo preferido terá uma aparência como a imagem abaixo:
+  ![](img/ColecoesREST.png)
+  - **Passos importantes!!**
+    - Siga a sequência de inserções: obs.: Troque as informações nos valores do JSON
+      - [CandidatePOST]
+        ```
+        {
+          "FullName": "Uncle Bob",
+          "ViceName": "Chuck",
+          "RegisterDate": "{% now 'iso-8601', '' %}",
+          "Label": 20
+        }
+        ```
+      - [VotePOST]
+      ```
+      {
+        "CandidateId": 1,
+        "VoteDate": "{% now 'iso-8601', '' %}"
+      }
+      ```
+      - [CandidateGET] - Verifica todos os Candidatos cadastrados
+      - [VotesGET] - Verifica todos os votos realizados
+      - [VotesCandidateGET] - Verifica todos os votos POR Candidato, utilizando como parâmetro o CandidateId
+      
+- Ao inserir as informações acesse o arquivo ***index.html*** para acessar a SPA Urna Eletrônica
+- Irá se deparar com algo algo do tipo:
+ ![](img/SPAUrna.png)
+  - obs.: Página em construção :rocket: ... não estranhe por enquanto :eyes:
+
+###Considerações
+
+Este foi um pequeno desafio, bem interessante para praticar e aplicar alguns conceitos no Backend com .Net quanto no Frontend (essa parte foi pura gambiarra :eyes::rofl:).
+
+Em breve irei finalizar este projeto :beers:
--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -12,79 +12,116 @@
 //     });
 // })();
 
-let table = document.createElement("table");
-let thead = document.createElement("thead");
-let tbody = document.createElement("tbody");
+GetCandidates();
+GetVotes();
 
-table.appendChild(thead);
-table.appendChild(tbody);
+// let table = document.createElement("table");
+// let thead = document.createElement("thead");
+// let tbody = document.createElement("tbody");
+//
+// table.appendChild(thead);
+// table.appendChild(tbody);
+//
+// document.getElementById("theadId").appendChild(table);
+//
+// // Creating and adding data to first row
+// let row_1 = document.createElement("tr");
+// let heading_1 = document.createElement("th");
+// heading_1.innerHTML = "Id";
+// let heading_2 = document.createElement("th");
+// heading_2.innerHTML = "Full Name";
+// let heading_3 = document.createElement("th");
+// heading_3.innerHTML = "Vice Name";
+// let heading_4 = document.createElement("th");
+// heading_4.innerHTML = "Data Register";
+// let heading_5 = document.createElement("th");
+// heading_5.innerHTML = "Label";
+//
+// row_1.appendChild(heading_1);
+// row_1.appendChild(heading_2);
+// row_1.appendChild(heading_3);
+// row_1.appendChild(heading_4);
+// row_1.appendChild(heading_5);
+// thead.appendChild(row_1);
+//
+// let rows = document.createElement("tr");
+// rows.appendChild(rows);
+// tbody.appendChild(rows);
 
-document.getElementById("theadId").appendChild(table);
-
-// Creating and adding data to first row
-let row_1 = document.createElement("tr");
-let heading_1 = document.createElement("th");
-heading_1.innerHTML = "Id";
-let heading_2 = document.createElement("th");
-heading_2.innerHTML = "Full Name";
-let heading_3 = document.createElement("th");
-heading_3.innerHTML = "Vice Name";
-let heading_4 = document.createElement("th");
-heading_4.innerHTML = "Data Register";
-let heading_5 = document.createElement("th");
-heading_5.innerHTML = "Label";
-
-row_1.appendChild(heading_1);
-row_1.appendChild(heading_2);
-row_1.appendChild(heading_3);
-row_1.appendChild(heading_4);
-row_1.appendChild(heading_5);
-thead.appendChild(row_1);
-
-let rows = document.createElement("tr");
-rows.appendChild(rows);
-tbody.appendChild(rows);
-
-function createGet(url) {
+function createGetCandidates(url) {
     let request = new XMLHttpRequest()
     request.open("GET", url, false)
     request.send()
     return request.responseText
 }
 
-function createLine(candidate) {
+function GetCandidates() {
+    let dataCandidates = createGetCandidates("http://localhost:5100/v1/candidates");
+    let candidates = JSON.parse(dataCandidates);
+    let tbodyCandidates = document.getElementById("tbodyCandidates");
+    candidates.forEach(element => {
+        let line = createLineCandidates(element);
+        tbodyCandidates.appendChild(line);
+    });
+}
+
+function createLineCandidates(candidates) {
     line = document.createElement("tr");
-    tdId = document.createElement("td");
     tdFullName = document.createElement("td");
     tdViceName = document.createElement("td");
-    tdRegisterDate = document.createElement("td");
     tdLabel = document.createElement("td");
-    
-    thId = "Id"
-    tdId.innerHTML = candidate.id
-    tdFullName.innerHTML = candidate.fullName
-    tdViceName.innerHTML = candidate.viceName
-    tdRegisterDate.innerHTML = candidate.registerDate
-    tdLabel.innerHTML = candidate.label
 
-    line.appendChild(tdId);
+    tdFullName.innerHTML = candidates.fullName
+    tdViceName.innerHTML = candidates.viceName
+    tdLabel.innerHTML = candidates.label
+
     line.appendChild(tdFullName)
     line.appendChild(tdViceName)
-    line.appendChild(tdRegisterDate)
     line.appendChild(tdLabel)
 
     return line;
 }
 
+function createGetVotes(url) {
+    let request = new XMLHttpRequest();
+    request.open("GET", url, false)
+    request.send()
+    return request.responseText
+}
 
-function main() {
-    let dataCandidates = createGet("http://localhost:5100/v1/candidates");
-    let candidates = JSON.parse(dataCandidates);
-    let tbody = document.getElementById("tableid");
-    candidates.forEach(element => {
-        let line = createLine(element);
-        tbody.appendChild(line);
+function GetVotes() {
+    let dataVotes = createGetVotes("http://localhost:5100/v1/votes");
+    let votes = JSON.parse(dataVotes);
+    let tbodyVotes = document.getElementById("tbodyVotes");
+    votes.forEach(element => {
+        let line = createLineVotes(element);
+        tbodyVotes.appendChild(line);
     });
 }
 
-main();
+function createLineVotes(votes) {
+    line = document.createElement("tr");
+    tdFullName = document.createElement("td");
+    tdLabel = document.createElement("td");
+    tdVoteDate = document.createElement("td");
+    tdTotalVotes = document.createElement("td")
+
+    let totalVotes;
+    for (let i = 0; i < votes.length; i++) {
+        votes.forEach(vote => {
+            totalVotes += vote
+        })
+    }
+    console.log(totalVotes)
+    
+    tdFullName.innerHTML = votes.candidate.fullName
+    tdLabel.innerHTML = votes.candidate.label
+    tdTotalVotes.innerHTML = totalVotes
+
+    line.appendChild(tdFullName)
+    line.appendChild(tdLabel)
+    line.appendChild(tdTotalVotes)
+
+    return line;
+}
+
